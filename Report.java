42271221ddb3c0b839e2c2ffcb5c7a0c07a7acf2
nikkodg <nikkdg@gmail.com>
2014-11-21 23:49:19 -0800

    Create Report.java

---
+++ Report.java
@@ -0,0 +1,41 @@
+package hw2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Report {
+
+	//Print most points
+	//Print best cell config 
+	//Prefromance Time
+	File file = new File("report.txt"); 
+	
+	
+    public Report(File file) throws IOException {
+        // TODO Auto-generated constructor stub
+    	if(!file.exists()){
+			file.createNewFile();
+		}
+    }
+
+    public void add(String message) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void write(Road mostpoints, CellNetwork bestCellconfig, long startT, long endT ) throws IOException {
+ 
+    	long totalT = endT - startT;
+    	FileWriter fw = new FileWriter(file);
+    	BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("Group: Andrew and Nikko" + "\n");
+		bw.write("Road: " + mostpoints.id + " = " + mostpoints.amountOfPoints + "\n");
+		bw.write("Best Cell Configuration: " + bestCellconfig + "\n");
+		bw.write("Performance time: " + totalT + "ms");
+		bw.flush();
+		bw.close();
+		}
+    }
+
