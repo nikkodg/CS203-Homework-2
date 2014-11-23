39a28af4ec241550fed77aa87e7711ed8d550a6c
nikkodg <nikkdg@gmail.com>
2014-11-22 16:24:09 -0800

    Update OSM.java

--- OSM.java
+++ OSM.java
@@ -1,85 +1,19 @@
-package hw1;
+package hw2;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.ArrayList; 
+
 
 
 public class OSM {
-	ArrayList<Point> points = new ArrayList<Point>();
-	HashMap<Integer, Point> hashPoints = new HashMap<Integer, Point>();
-	ArrayList<Integer> refs = new ArrayList<Integer>();
-	
-	public void readOSMFormat(File file){
-		//extract boundaries
-//		try{
-//			Scanner scanner = new Scanner(file);
-//			while(scanner.hasNext()){
-//				String line = scanner.nextLine().replace("^\\s+", "");
-//				if(line.startsWith("<bounds")){
-//					double xmin = Double.parseDouble(extractStringFromVal(line, "minlat"));
-//					double ymin = Double.parseDouble(extractStringFromVal(line, "minlon"));
-//					double xmax = Double.parseDouble(extractStringFromVal(line, "maxlat"));
-//					double ymax = Double.parseDouble(extractStringFromVal(line, "maxlon"));
-////					Boundaries.update(line);
-//					System.out.println(xmin);
-//				}
-//				}
-//			scanner.close();
-//		}catch (Exception ex){
-//			return;
-//		}	
-		
-		try{
-			Scanner scanner = new Scanner(file);
-			while(scanner.hasNext()){
-				String line = scanner.nextLine().replace("^\\s+", "");
-				if(line.startsWith("<node")){
-//			int id = Integer.parseInt(extractStringFromVal(line, "id"));
-//			double x = Double.parseDouble(extractStringFromVal(line, "lat"));
-//			double y = Double.parseDouble(extractStringFromVal(line, "lon"));
-			Point p = new Point(line, true);
-			points.add(p);
-			hashPoints.put(p.id, p);
-			System.out.println(p);
-			
-		}}
-			
-		}catch (Exception ex){
-			return;
-		}
-//		
-//		try{
-//			Scanner scanner = new Scanner(file);
-//			while(scanner.hasNext()){
-//				String line = scanner.nextLine().replace("^\\s+", "");
-//		 if(line.startsWith("<way")){
-//			int id = Integer.parseInt(extractStringFromVal(line, "id"));
-//			
-//		}}
-//			
-//		}catch (Exception ex){
-//			return;
-//		}
-//		
-//		try{
-//			Scanner scanner = new Scanner(file);
-//			while(scanner.hasNext()){
-//				String line = scanner.nextLine().replace("^\\s+", "");
-//			if(line.startsWith("<nd")){
-//			int ref = Integer.parseInt(extractStringFromVal(line, "ref"));
-//			refs.add(ref);
-//			System.out.println(ref);
-//			
-//			}}}catch (Exception ex){
-//				return;
-//			}
-		
-	}
-	
-	
-	static String extractStringFromVal(String haystack, String needle) {
+    private static Scanner scanner;
+    public static HashMap<Long,Road> roads;
+    public static HashMap<Long, Point> points;
+
+    static String extractStringFromVal(String haystack, String needle) {
         String[] array = haystack.split("\\s+");
         for (String a : array) {
             String[] b = a.split("=");
@@ -89,22 +23,117 @@ public class OSM {
             String key = b[0];
             String val = b[1];
             if(needle.compareToIgnoreCase(key) == 0) {
-                return val.replaceAll("\"", "");
+                return val.replaceAll("\"", "").replaceAll("\\/", "").replaceAll(">", "");
             }
         }
 
         return null;
     }
+    
+    public static void extractBoundaries(File file) {
+        try {
+            scanner = new Scanner( file );
+            while ( scanner.hasNext() ) {
+                String line = scanner.nextLine();
+                if (line.contains("<bounds")) {
+                    Boundaries.update(line);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(-1); 
+        }
+    }
 
-	public void print(){
-		
-		for(int i =0; i <points.size(); i++){
-		System.out.println(points);
-		}
-		
-		for(int i=0; i<refs.size(); i++){
-			System.out.println(refs);
-		}
-	}
+    public static HashMap<Long,Point> extractNodes(File file) {
+        HashMap<Long,Point> nodes = new HashMap<Long,Point>();
+        
+        try {
+            scanner = new Scanner( file );
+            while ( scanner.hasNext() ) {
+                String line = scanner.nextLine();
+                if (line.contains("<node")) {
+                    Point p = new Point(line, true);
+                    nodes.put( (long)p.id, p);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+        
+        points = new HashMap<Long, Point>(nodes);
+        return nodes;
+    }
+
+    public static HashMap<Long,Road> 
+    extractWays(File file, HashMap<Long, Point> nodes) {
+        HashMap<Long,Road> ways = new HashMap<Long,Road>();
+        
+        try {
+            scanner = new Scanner( file );
+            while ( scanner.hasNext() ) {
+                String line = scanner.nextLine();
+                if (line.contains("<way")) {
+                    Road road = new Road(line, true);
+                    while (!line.contains("</way")) {
+                        line = scanner.nextLine();
+                        if (line.contains("<nd")) {
+                            long nodeIdRef = Long.parseLong(OSM.extractStringFromVal(line, "ref"));
+                            Point p = nodes.get(nodeIdRef);
+                            assert (p != null);
+                            road.points.add( p );
+                        }
+                    }
+                    road.build(road.points);
+                    ways.put( road.id, road );
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+        roads = new HashMap<Long, Road>(ways);
+        //System.out.println(ways);
+        return ways;
+    }
+    
+    public static void OSM2Text(File osmFile, File textFile) {
+        extractBoundaries(osmFile);
+        HashMap<Long,Point> nodes = extractNodes(osmFile);
+        HashMap<Long,Road> ways = extractWays(osmFile, nodes);
+        
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(textFile));
+            writer.write("boundaries: " + Boundaries.xmin + " " + Boundaries.ymin + " " + Boundaries.xmax + " " +  Boundaries.ymax + "\n");
+            for (Point p : nodes.values()) {
+                writer.write(p.toString() + "\n");
+            }
+            for (Road r : ways.values()) {
+                writer.write(r.toString() + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (Exception e) {
+            }
+        }
+        
+    }
+    
+//    public static void main(String[] args) {
+//        try {
+//            File osmFile = new File(args[0]);
+//            File textFile = new File(args[1]);
+//            OSM2Text(osmFile, textFile);
+//            
+//        } catch (Exception ex) {
+//            System.out.println("usage: prog <osm file> <text file>");
+//        }
+//    }
+
 }
-
